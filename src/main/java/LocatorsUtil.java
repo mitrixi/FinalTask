0170cd5b6790ed7257e90359731b07bbd95f46b5
dbalalin <dbalalin@mmtr.ru>
2020-09-30 18:00:30 +0300

    chanded locator 'comparison'

--- src/main/java/LocatorsUtil.java
+++ src/main/java/LocatorsUtil.java
@@ -6,13 +6,13 @@ import static com.codeborne.selenide.Selenide.$x;
 
 public class LocatorsUtil {
 
-    public static SelenideElement productCatalog = $x("//span[text()='Каталог товаров']");
+    public static SelenideElement productCatalog = $x("//span[text()='Каталог']");
     public static SelenideElement electronics = $x("//img[@alt='Электроника']");
     public static SelenideElement mobilePhones = $x("//a[text()='Мобильные телефоны']");
 
     public static SelenideElement sortPhonesByPrice = $x("//button[text()='по цене']");
     public static SelenideElement closePopup = $x("//div[@data-apiary-widget-name='@market/PopupInformer']//button");
-    public static SelenideElement pushButtonComparing = $$x("//span[text()='Сравнение']").first();
+    public static SelenideElement pushButtonComparing = $$x("//*[name()='svg' and @title='Сравнение'] ").first();
     public static SelenideElement isPhoneAddedToComparing = $x("//div[contains(text(), 'добавлен к сравнению')]");
 
     public static ElementsCollection isThereTwoPhonesInList = $$x("//a[contains(text(), 'Смартфон')]");
